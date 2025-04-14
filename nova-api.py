658ca4360c40031513e7a02189dd5e5b9d211eb5
AaronCutipa <aaron.canaviri@sptech.school>
2025-04-13 21:01:50 -0300

    feat: função de coleta de dados para entidade historico e alerta

--- nova-api.py
+++ nova-api.py
@@ -3,6 +3,7 @@ import psutil
 import platform
 import time
 import subprocess
+import cpuinfo
 from datetime import datetime
 
 # Conexão com o banco de dados
@@ -52,13 +53,14 @@ memoria_total = psutil.virtual_memory().total / (1024 ** 3)
 # Total de disco em GB
 disco_total = psutil.disk_usage('/').total / (1024 ** 3)
 # Nome do processador
-processador = platform.processor()
+processador = cpuinfo.get_cpu_info()['brand_raw']
 # Arquitetura do sistema (32 bits ou 64 bits)
 arquitetura = platform.machine()
 # Nome do sistema operacional
 sistema_operacional = platform.system()
 # Versão do sistema operacional
 versao_sistema = platform.version()
+sistema_detalhado = platform.platform()
 # Número serial da máquina Windows
 output = subprocess.check_output("wmic bios get serialnumber").decode().split("\n")
 serial_number = output[1].strip()
@@ -87,33 +89,19 @@ def cadastrar_maquina():
     setor = input("Qual é o setor da sua máquina?: ").strip()
     print("\n=========================================================================")
 
-    windowsBo = False;
-    linuxBo = False;
-    macosBo = False;
-    if sistema_operacional == "Windows":
-        windowsBo = True;
-    elif sistema_operacional == "Linux":
-        linuxBo = True;
-    elif sistema_operacional == "Darwin":
-        macosBo = True;
-    print("Identificamos o modelo do sistema operacional da sua maquina {}".format(sistema_operacional))
+    print("Identificamos o modelo do sistema operacional da sua maquina {}".format(sistema_detalhado))
     
-    if windowsBo == True:
-        def get_serial():
+    def get_serial():
+        if sistema_operacional == "Windows":
             result = subprocess.check_output("wmic bios get serialnumber", shell=True)
             return result.decode().split("\n")[1].strip()
-    elif linuxBo == True:
-        def get_serial():
+        elif sistema_operacional == "Linux":
             result = subprocess.check_output("sudo dmidecode -s system-serial-number", shell=True)
             return result.decode().strip()
-    elif macosBo == True:
-        def get_serial():
+        elif sistema_operacional == "Darwin":
             result = subprocess.check_output("system_profiler SPHardwareDataType | grep 'Serial Number'", shell=True)
             return result.decode().split(":")[1].strip()
-    else:
-        def get_serial():
-            result = "Não encontrado"
-            return result
+        return "Não encontrado"
 
     print("Identificamos o número serial da sua maquina {}".format(get_serial()))
     print("\n=========================================================================")
@@ -127,31 +115,40 @@ def cadastrar_maquina():
     print("Cadastrando máquina...")
     print("\n=========================================================================")
     sql = "INSERT INTO maquina (modelo, so, serial_number, status, setor, fk_maquina_empresa) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (modelo_maquina, sistema_operacional, serial_number, status, setor, 1)
+    val = (modelo_maquina, sistema_detalhado, serial_number, status, setor, 1)
     mycursor.execute(sql,val)
     cnx.commit()
     # cnx.close()
 
+#######################################################################
     print("Vamos cadastrar os componentes da máquina...")
+    desc_cpu = cpuinfo.get_cpu_info()['brand_raw']
+    print(desc_cpu)
 
 
     def identificar_componente():
         componentes = []
 
         componentes.append({
-            "tipo": "CPU",
+            "tipo": "cpu_percent",
             "modelo": processador,
             "minimo": 10.0,
             "maximo": 40.0
         })
         componentes.append({
-            "tipo": "RAM",
+            "tipo": "cpu_freq",
+            "modelo": processador,
+            "minimo": 10.0,
+            "maximo": 40.0
+        })
+        componentes.append({
+            "tipo": "virtual_memory.percent",
             "modelo": "DDR4",
             "minimo": 10.0,
             "maximo": 70.0
         })
         componentes.append({
-            "tipo": "DISK",
+            "tipo": "disk_usage.percent",
             "modelo": "Samsung SSD 860 EVO 500GB",
             "minimo": 20.0,
             "maximo": 80.0
@@ -171,8 +168,9 @@ menu_inicial()
 
 
 def monitoramento_em_tempo_real():
+    print("\n=========================================================================")
 
-    escolher_maquina = input("Qual máquina você deseja monitorar?:" ).strip()
+    escolher_maquina = input("Qual máquina você deseja monitorar? (digite o id da maquina):" ).strip()
     sql = "SELECT id_maquina, modelo, so, serial_number, status, setor FROM maquina WHERE id_maquina = %s"
     val = (escolher_maquina, )
     mycursor.execute(sql,val)
@@ -180,6 +178,7 @@ def monitoramento_em_tempo_real():
     maquina_escolhida = mycursor.fetchone()
 
     if maquina_escolhida:
+        print("\n=========================================================================")
         print("Máquina encontrada:")
         print(f"ID: {maquina_escolhida['id_maquina']}")
         print(f"Modelo: {maquina_escolhida['modelo']}")
@@ -188,93 +187,91 @@ def monitoramento_em_tempo_real():
         print(f"Status: {maquina_escolhida['status']}")
         print(f"Setor: {maquina_escolhida['setor']}")
     else:
+        print("\n=========================================================================")
         print("Nenhuma máquina encontrada com esse ID.")
 
-    
-
     cnx.commit()
+    prog_monitoracao = input("Prosseguir com a monitoração?(digite y ou qualquer outra tecla para escolher outra maquina)\n")
 
+    
+    if prog_monitoracao == "y":
+        print("\nIniciando monitoramento em tempo real...")
+            
+        funcoes_metrica = {
+            "cpu_percent": lambda: psutil.cpu_percent(interval=1),
+            "cpu_freq": lambda: psutil.cpu_freq().current,
+            "virtual_memory.percent": lambda: psutil.virtual_memory().percent,
+            "disk_usage.percent": lambda: psutil.disk_usage('/').percent
+        }
 
-    # print("\nIniciando monitoramento em tempo real...")
-    # print("Coletando dados de monitoramento...")
-    # while True:
-    #     # Porcentagem de uso da CPU nos últimos 1 segundo
-    #     cpu_porcentagem = psutil.cpu_percent(interval=1)
-    #     # Porcentagem de memória RAM atualmente em uso
-    #     memoria_porcentagem = psutil.virtual_memory().percent
-    #     # Porcentagem de espaço utilizado no disco principal
-    #     disco_percentagem = psutil.disk_usage('/').percent
-    #     # Dados de tráfego de rede  
-    #     rede = psutil.net_io_counters()
-    #     # Quantidade total de bytes enviados pela rede desde a inicialização do sistema
-    #     bytes_enviados = rede.bytes_sent  
-    #     # Quantidade total de bytes recebidos pela rede desde a inicialização do sistema
-    #     bytes_recebidos = rede.bytes_recv  
-    #     # Número total de pacotes enviados pela rede desde a inicialização do sistema
-    #     pacotes_enviados = rede.packets_sent  
-    #     # Número total de pacotes recebidos pela rede desde a inicialização do sistema
-    #     pacotes_recebidos = rede.packets_recv  
+        sql = "SELECT id_componente, tipo, minimo, maximo FROM componente WHERE fk_componente_maquina = %s"
+        val = (escolher_maquina,)
+        mycursor.execute(sql, val)
+        componentes = mycursor.fetchall()
 
-        # print("=========================================================================")
-        # print("Dados de monitoramento")
-        # print("Uso da CPU: {:.2f}%".format(cpu_porcentagem))
-        # print("Uso da Memória RAM: {:.2f}%".format(memoria_porcentagem))
-        # print("Uso do Disco: {:.2f}%".format(disco_percentagem))
-        # print("Total de bytes enviados pela rede: {} bytes".format(bytes_enviados))
-        # print("Total de bytes recebidos pela rede: {} bytes".format(bytes_recebidos))
-        # print("Total de pacotes enviados pela rede: {}".format(pacotes_enviados))
-        # print("Total de pacotes recebidos pela rede: {}".format(pacotes_recebidos))
-        # print("=========================================================================")
+        while True:
+            for comp in componentes:
+                id_comp = comp['id_componente']
+                tipo = comp['tipo']
+                minimo = comp['minimo']
+                maximo = comp['maximo']
 
+                if tipo in funcoes_metrica:
+                    valor = funcoes_metrica[tipo]()
+                    dataCaptura = datetime.now()
 
+                    if valor < minimo or valor > maximo:
+                        sql_insert = "INSERT INTO alerta (data_captura, valor, fk_alerta_componente) VALUES (%s, %s, %s)"
+                        val_insert = (dataCaptura, valor, id_comp)
+                        mycursor.execute(sql_insert, val_insert)
+                        cnx.commit()
+                    else:
+                        sql_insert = "INSERT INTO historico (data_captura, valor, fk_historico_componente) VALUES (%s, %s, %s)"
+                        val_insert = (dataCaptura, valor, id_comp)
+                        mycursor.execute(sql_insert, val_insert)
+                        cnx.commit()
 
-        # Armazenado no histórico e alertas
-        # dataCaptura = datetime.now()
+                else:
+                    print(f"[ERRO] Tipo '{tipo}' não está mapeado nas funções.")
 
-        
-        # sql = "INSERT INTO historico (data_captura, valor, fk_historico_componente) VALUES (%s,%s,%s)"
-        # val = (dataCaptura, valor, fkComponete)
-
-        # if componente < minimo or componente > maximo:
-            # sql = "INSERT INTO alerta (data_captura, valor, fk_alerta_componente) VALUES (%s,%s,%s)"
-            # val = (dataCaptura, valor, fkComponete)
-        
-
-
-        # time.sleep(1)
+            time.sleep(5)
+    else:
+        print("\n=========================================================================")
+        print("Nenhuma máquina encontrada com esse ID.")
+        return
 
 def encerrar_servico():
-    print("\nServiço encerrado. Obrigado por usar o sistema da InnovaAir.")
+    print("\nServiço encerrado. Obrigado por usar o sistema Sentinela.")
     exit()
 
 
 def executar():
-    inicio = menu_inicial()
+    while True:
+        inicio = menu_inicial()
 
-    if inicio == 'y':
+        if inicio != 'y':
+            break
+
         nome = coleta_nome_usuario()
         escolha = escolha_usuario(nome)
 
         if escolha == '1':
             menu_informacoes_maquina()
-            
-            encerrar_servico()
+
         elif escolha == '2':
             cadastrar_maquina()
-            # menu_inicial()
-            # encerrar_servico()
 
         elif escolha == '3':
             monitoramento_em_tempo_real()
 
         elif escolha == '0':
-            encerrar_servico()
+            break
 
         else:
-            print("\nOpção inválida! O serviço será encerrado.")
-            encerrar_servico()
-    else:
-        encerrar_servico()
+            print("\nOpção inválida! Tente novamente.\n")
+
+    print("\nServiço encerrado. Obrigado por usar o sistema Sentinela.")
+
 
 executar()
 
